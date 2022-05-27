386c7d91017608a58c26fae96022d1b64f9f7fa8
Pedro Aldo <pedroaldo2020@gmail.com>
2022-05-27 17:01:57 -0300

    Cria o Header desktop e mobile

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const buttonOpenCategories = document.getElementById('header-button-mobile')
+    const buttonOpenCategories2 = document.getElementById('header-button-mobile2')
+
+    function openMenu () {
+        const nav = document.getElementById('header-nav');
+        nav.classList.toggle('menu-active');
+        const button = document.getElementById('header-button-mobile3')
+        button.classList.toggle('menu-desactive')
+    }
+
+    buttonOpenCategories.addEventListener('click', openMenu);
+    buttonOpenCategories2.addEventListener('click', openMenu);
+});
