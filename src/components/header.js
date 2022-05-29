3b18a168fba7a14a107cc0a6661a767fc101bd87
Pedro Aldo <pedroaldo2020@gmail.com>
2022-05-29 12:32:53 -0300

    Cria a parte de lançamentos desktop e mobile, sem funcionalidade slide (por enquanto)

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const buttonOpenCategories = document.getElementById('header-button-mobile')
-    const buttonOpenCategories2 = document.getElementById('header-button-mobile2')
+    const buttonOpenCategories = document.getElementById('header-button-mobile');
+    const buttonOpenCategories2 = document.getElementById('header-button-mobile2');
 
     function openMenu () {
         const nav = document.getElementById('header-nav');
         nav.classList.toggle('menu-active');
-        const button = document.getElementById('header-button-mobile3')
-        button.classList.toggle('menu-desactive')
+        const button = document.getElementById('header-button-mobile3');
+        button.classList.toggle('menu-desactive');
     }
 
     buttonOpenCategories.addEventListener('click', openMenu);
